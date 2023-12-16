4bf7c86e3c40b5b3fbc1932875e830e9030dcfa9
zaheralmajed <zaher.almajed@gmail.com>
2023-12-16 10:52:47 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import ButtonMain from "./components/buttonMain";
 export default function Home() {
   return (
     <div className="w-full max-w-md w-full">
-      <ButtonMain LINK={"/shuffle"} TEXT={"Shuffle"} DESC={"Randomize the Winner List"} />
-      <ButtonMain LINK={"/winner"} TEXT={"Winner"} DESC={"Find the lucky winner from the List"} />
+      <ButtonMain LINK={"/shuffle"} TEXT={"Shuffle"} DESC={"Randomize the Winners List"} />
+      <ButtonMain LINK={"/winner"} TEXT={"Winner"} DESC={"Find the lucky winner from a List"} />
     </div>
   );
 }
